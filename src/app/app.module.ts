f25639b0add84ae8e5ababf3db667c108594dfef
CGAnVil <hoangminh.tlu@gmail.com>
2022-05-12 09:54:30 +0700

    fix component url

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomepageComponent } from './homepage/homepage.component';
-import { PostCreateComponent } from './post/post-create/post-create.component';
 import {CKEditorModule} from '@ckeditor/ckeditor5-angular';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomepageComponent,
-    PostCreateComponent
   ],
   imports: [
     BrowserModule,
