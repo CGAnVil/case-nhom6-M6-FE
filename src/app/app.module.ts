2dac4dc1f5c16ed3408427472311463cf9713cfb
TriHoangZXC <hoanghuutri97@gmail.com>
2022-05-12 10:33:41 +0700

    refactor project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,20 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {CKEditorModule} from '@ckeditor/ckeditor5-angular';
+import { NavbarComponent } from './shared/admin/navbar/navbar.component';
+import { SidebarComponent } from './shared/admin/sidebar/sidebar.component';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavbarComponent,
+    SidebarComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CKEditorModule
+    CKEditorModule, HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
